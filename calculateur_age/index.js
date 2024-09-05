5e5326e8bcd554e4528903ca23c8ecd322f96639
rylescargot <ryles@ngine.tv>
2024-09-05 11:14:42 +0200

    Ajout de commentaires

--- calculateur_age/index.js
+++ calculateur_age/index.js
@@ -2,6 +2,10 @@ const dateE1 = document.getElementById("date");
 const submitE1 = document.getElementById("submit");
 const resultatE1 = document.getElementById("resultat");
 
+
+
+/*Le programme examine cette fonction en premier (pour récupérer le val_age), puis repasse une 2eme fois après la 2eme fonction.
+Elle sert à récupérer la valeur de l'âge obtenu grâce à la première fonction pour la ressortir en HTML.*/
 function calcul_age() {
     const val_age = dateE1.value;
     if (val_age === "") {
@@ -14,6 +18,10 @@ function calcul_age() {
 }
 
 
+
+
+/* Il me semble que le programme examine cette fonction en premier. 
+Cette fonction sert à soustraire la date actuelle à la date de naissance de la personne.*/
 function getAge(val_age) {
     const currentDate = new Date();
     const date_age = new Date(val_age);
@@ -21,7 +29,7 @@ function getAge(val_age) {
     const month = currentDate.getMonth() - date_age.getMonth();
 
     if (month < 0 || (month === 0 && currentDate.getDate() < date_age.getDate())) {
-        age--;
+        alert("Rentre un âge correct");
     }
 
     return age;
